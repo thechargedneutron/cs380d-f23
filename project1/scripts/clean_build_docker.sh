01c14af37707365b7500eda2efe260f016ca28ca
ash <ash@master0.cluster.local>
2023-09-28 15:01:29 -0400

    my local changes to make the code run

--- project1/scripts/clean_build_docker.sh
+++ project1/scripts/clean_build_docker.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'sekwonlee' | awk '{print $2}')
+sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'thechargedneutron' | awk '{print $2}')
 
 cd dockerfiles
 
-sudo docker build . -f base.dockerfile -t sekwonlee/kvs:base --network=host
-sudo docker push sekwonlee/kvs:base
+sudo docker build . -f base.dockerfile -t thechargedneutron/kvs:base --network=host
+sudo docker push thechargedneutron/kvs:base
 
-sudo docker build . -f client.dockerfile -t sekwonlee/kvs:client --network=host
-sudo docker push sekwonlee/kvs:client
+sudo docker build . -f client.dockerfile -t thechargedneutron/kvs:client --network=host
+sudo docker push thechargedneutron/kvs:client
 
-sudo docker build . -f frontend.dockerfile -t sekwonlee/kvs:frontend --network=host
-sudo docker push sekwonlee/kvs:frontend
+sudo docker build . -f frontend.dockerfile -t thechargedneutron/kvs:frontend --network=host
+sudo docker push thechargedneutron/kvs:frontend
 
-sudo docker build . -f server.dockerfile -t sekwonlee/kvs:server --network=host
-sudo docker push sekwonlee/kvs:server
+sudo docker build . -f server.dockerfile -t thechargedneutron/kvs:server --network=host
+sudo docker push thechargedneutron/kvs:server
 
 cd ..
